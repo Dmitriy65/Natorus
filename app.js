b9919470acf4eeadccd70386bb05d074b3bd661b
Dmitriy65 <mr.dima19980@gmail.com>
2019-10-28 22:06:45 +0300

    added signup and login features with jwt token

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const morgan = require('morgan');
-const AppError = require('./utils/apiFeature');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -14,10 +13,6 @@ app.use(express.json());
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
-app.all('*', (req, res, next) => {
-  next(new AppError(`cant found route for ${req.originalUrl} url`, 404));
-});
 
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,22 @@
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
+const AppError = require('./../utils/appError');
+
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
 
 exports.signup = async (req, res, next) => {
-  const newUser = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm
-  });
-
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
-  });
-
   try {
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm
+    });
+
+    const token = signToken(newUser._id);
+
     res.status(201).json({
       status: 'success',
       token,
@@ -25,6 +28,34 @@ exports.signup = async (req, res, next) => {
     res.status(404).json({
       status: 'fail',
       message: 'signup error!'
+    });
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return next(new AppError(`Please provide password or email`, 400));
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+    const correct = await user.correctPassword(password, user.password);
+
+    if (!user || !correct) {
+      return next(new AppError(`Please check your password or email`, 401));
+    }
+
+    const token = signToken(user._id);
+    res.status(200).json({
+      status: 'success',
+      token
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'login error!'
     });
   }
 };
